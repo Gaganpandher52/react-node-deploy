e66612a53c7bcf6de4fae9c43a96bce83c685f01
gaganpandher52 <gaganpandher52@gmail.com>
2019-10-05 17:11:33 -0600

    intial

---
+++ index.js
@@ -0,0 +1,35 @@
+const Express = require('express');
+const App = Express();
+const cors = require('cors');
+const axios = require('axios');
+const PORT = process.env.PORT || 8080;
+
+// Express Configuration
+// App.use(BodyParser.urlencoded({ extended: false }));
+// App.use(Express.static('public'));
+
+// // Sample GET route
+// App.get('/api/data', (req, res) => res.json({
+//   message: "Seems to work!",
+// }));
+App.use(cors());
+
+App.get('/api',(req,res)=>{
+  const user = req.query.name || 'lol'
+  axios.get('http://interviewtest.replicon.com/employees/')
+  .then(response =>{
+    res.json(response.data);
+  })
+});
+
+if(process.env.NODE_ENV ==='production'){
+  App.use(express.static('client/build'));
+  App.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+App.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+});
